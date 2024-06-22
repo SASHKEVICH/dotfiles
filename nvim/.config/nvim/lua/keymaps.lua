1d3884a8ec2b36b60d489f419876f3d56bf78c28
Aleksandr Bekrenev <praise2001@yandex.ru>
2024-06-22 12:02:32 +0700

    feat(): new vim keybinding

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<C-q>", ":wqa!<CR>", { desc = "Quit" })
 vim.keymap.set("t", "T<Esc>", "<C-\\><C-n>", { desc = "Go to normal mode in terminal" })
+vim.keymap.set("n", ",,", ":nohl<cr>", { desc = "Remove search hightligthing" })
